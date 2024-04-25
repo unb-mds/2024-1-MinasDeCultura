d3181d4bedcbdd7dec54d200f475a8c6485e695b
devMarcosVM <marcosvieira.2003@gmail.com>
2024-04-24 22:40:39 -0300

    feat: Criando o Header e filtragem de pesquisa

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,77 @@
 import Image from "next/image";
+import lupa from '@/assets/lupa.png';
+import calendario from '@/assets/calendario.png';
+import arrow from '@/assets/arrow.png';
+import local from '@/assets/local.png';
 
 export default function Home() {
   return (
-    <main className="bg-gray-10 flex flex-col items-center justify-center min-h-screen p-24">
-      <div>
-          <h1 className="font-bold text-2xl">Cultura de Minas</h1>
+    <main className="bg-primary-gray flex flex-col items-center min-h-screen p-[44px]">
+      <div className="w-[1390px] bg-primary-white border rounded-lg flex flex-col items-center justify-center px-[70px] py-[23px]">
+        <h1 className="font-sans text-4xl text-center mb-[50px]">
+          Realize sua pesquisa filtrando os resultados desejados:
+        </h1>
+        <div className="flex gap-[34px]">
+          <ul className="flex gap-4">
+            <li className="relative flex items-center">
+              <Image
+                src={lupa}
+                alt="lupa"
+                width={24}
+                height={24}
+              />
+              <input
+                type="text"
+                placeholder="Local"
+                className="flex-1 px-4 py-2 "
+              />
+              <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
+            </li>
+            
+            <li className="relative flex items-center">
+              <Image
+                src={local}
+                alt="local"
+                width={24}
+                height={24}
+              />
+              <input
+                type="text"
+                placeholder="Data"
+                className="flex-1 px-4 py-2 "
+              />
+              <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
+            </li>
+
+            <li className="relative flex items-center">
+              <Image
+                src={calendario}
+                alt="calendario"
+                width={24}
+                height={24}
+              />
+              <input
+                type="text"
+                placeholder="Outro campo"
+                className="flex-1 px-4 py-2"
+              />
+              <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
+            </li>
+            <li className="relative flex items-center">
+              <Image
+                src={arrow}
+                alt="arrow"
+                width={24}
+                height={24}
+              />
+              <button className="w-[177px] h-[48px] px-4 py-2 bg-primary-red text-white rounded-lg">
+                Buscar
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
+      <div className="w-[1382px] h-[485px] bg-primary-white border rounded-lg mt-8"></div>
     </main>
   );
 }
--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,16 +1,40 @@
 import Image from "next/image";
-
 import Logo from '@/assets/Logo.png';
 
-export function Header(){
-    return(
-        <header className="w-full h-22 bg-white">
-            <div>
-                <Image
-                    src={Logo}
-                    alt='Logo'
-                />
-            </div>
-        </header>
+export function Header() {
+    return (
+        <div className="flex justify-center items-center  bg-primary-gray">
+            <header className="flex w-[1280px] h-22 bg-primary-white relative px-[120px] mx-auto items-center justify-between rounded-xl">
+                <div className="max-w-[348px]">
+                    <Image
+                        src={Logo}
+                        alt='Logo'
+                    />
+                </div>
+                <ul className="flex gap-[40px] font-sans 2xl">
+                    <li>
+                        <button>
+                            <span>
+                                HOME
+                            </span>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <span>
+                                SOBRE
+                            </span>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <span>
+                                PESQUISA FILTRADA
+                            </span>
+                        </button>
+                    </li>                       
+                </ul>
+            </header>
+        </div>
     )
 }
