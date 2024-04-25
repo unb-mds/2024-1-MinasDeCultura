0c30e10ae73da3cdd181ef714dbdc8c2bd9d3307
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-04-25 06:41:50 -0300

    refactor: form responsivity and styling

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,79 +1,51 @@
-import Image from "next/image";
-import lupa from '@/assets/lupa.png';
-import calendario from '@/assets/calendario.png';
-import arrow from '@/assets/arrow.png';
-import local from '@/assets/local.png';
+import { Search, MapPin, CalendarClock, MoveRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="bg-primary-gray flex flex-col items-center min-h-screen p-[44px]">
-      <div className="w-[1390px] bg-primary-white border rounded-lg flex flex-col items-center justify-center px-[70px] py-[23px]">
+    <main className="bg-primary-gray flex flex-col items-center min-h-screen p-12">
+      <div className="container bg-primary-white border rounded-lg flex flex-col items-center justify-center lg:p-12 p-8">
         <h1 className="font-sans text-4xl text-center mb-[50px]">
           Realize sua pesquisa filtrando os resultados desejados:
         </h1>
-        <div className="flex gap-[34px]">
-          <ul className="flex gap-4">
-            <li className="relative flex items-center">
-              <Image
-                src={lupa}
-                alt="lupa"
-                width={24}
-                height={24}
-              />
-              <input
-                type="text"
-                placeholder="Assunto"
-                className="flex-1 px-4 py-2 "
-              />
-              <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
-            </li>
-            
-            <li className="relative flex items-center">
-              <Image
-                src={local}
-                alt="Local"
-                width={24}
-                height={24}
-              />
-              <input
-                type="text"
-                placeholder="Local"
-                className="flex-1 px-4 py-2 "
-              />
-              <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
-            </li>
+        <ul className="flex lg:flex-row w-full justify-center flex-col gap-4">
+          <li className="relative flex items-center">
+            <Search className="w-6 h-6" color="#ED1C24" />
+            <input
+              type="text"
+              placeholder="Assunto"
+              className="w-full px-4 py-2 "
+            />
+            <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
+          </li>
 
-            <li className="relative flex items-center">
-              <Image
-                src={calendario}
-                alt="calendario"
-                width={24}
-                height={24}
-              />
-              <input
-                type="text"
-                placeholder="Data"
-                className="flex-1 px-4 py-2"
-              />
-              <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
-            </li>
-            <li className="relative flex items-center">
-              <Image
-                src={arrow}
-                alt="arrow"
-                width={24}
-                height={24}
-              />
-              <button className="w-[177px] h-[48px] px-4 py-2 bg-primary-red text-white rounded-lg">
-                Buscar
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="w-[1382px] h-[485px] bg-primary-white border rounded-lg mt-8">
+          <li className="relative flex items-center">
+            <MapPin className="w-6 h-6" color="#ED1C24" />
+            <input
+              type="text"
+              placeholder="Local"
+              className="w-full px-4 py-2 "
+            />
+            <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
+          </li>
 
+          <li className="relative flex items-center">
+            <CalendarClock className="w-6 h-6" color="#ED1C24" />
+            <input
+              type="text"
+              placeholder="Data"
+              className="w-full px-4 py-2"
+            />
+            <div className="border-b border-black absolute left-0 right-0 bottom-0"></div>
+          </li>
+          <li>
+            <button className="flex flex-row items-center gap-2 px-6 py-4 w-full md:justify-start justify-center bg-primary-red text-white rounded-lg">
+              Buscar
+              <MoveRight className="w-6 h-6" color="#fff" />
+            </button>
+          </li>
+        </ul>
       </div>
+      <div className="container h-[485px] bg-primary-white border rounded-lg mt-8"></div>
     </main>
   );
 }
