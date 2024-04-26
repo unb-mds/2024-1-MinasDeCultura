e8d5140778e6a1d9452d8eba421b5604f3e26f74
devMarcosVM <marcosvieira.2003@gmail.com>
2024-04-26 11:19:12 -0300

    fix: .gitignore

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { Header } from "@/components/Header";
-import Busca from "@/components/Busca"
 
 
 const inter = Inter({ subsets: ["latin"] });
--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import Logo from '@/assets/Logo.png';
+import Sobre from "../app/Sobre/page";
+import Link from 'next/link';
+
 
 export function Header() {
+
     return (
             <header className="flex w-full h-22 bg-primary-white relative px-44 mx-auto items-center justify-between rounded-xl">
                 <div>
