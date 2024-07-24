16793cb546e977dae2531d4be8ead8f76bf6aabb
devMarcosVM <marcosvieira.2003@gmail.com>
2024-07-24 11:46:41 -0300

    style: responsividade para todos os aparelhos na header

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,45 +1,36 @@
 import Image from "next/image";
-import Logo from '@/assets/Logo.png';
+import Logo from '@/assets/logo_vermelha.png';
 import Link from 'next/link';
+import { Menu } from "lucide-react";
 
 export function Header() {
     return (
-        <header className="flex w-full h-22 bg-primary-white relative px-44 mx-auto items-center justify-between rounded-xl">
-            <div>
-                <Image
-                    src={Logo}
-                    alt='Logo'
-                />
+        <header className="flex w-full h-22 bg-primary-white relative px-5 gap-20 sm:px-20 lg:px-44 mx-auto items-center justify-between">
+            <div className="flex flex-row items-center justify-center gap-4">
+                    <Menu className="w-6 h-6 lg:hidden gap-4" />
+                    <Image className="size-14 lg:size-20"
+                        src={Logo}
+                        alt='Logo'
+                    />
+                    <h1 className="text-center text-2xl sm:text-3xl font-DMsans "><strong>Minas</strong>deCultura</h1>
             </div>
-            <ul className="flex gap-10 font-sans 2xl">
+            <ul className="hidden lg:flex lg:gap-10 lg:font-DMsans">
                 <li>
                     <Link href="/">
-                        <button>
-                            <span>
-                                HOME
-                            </span>
-                        </button>
+                        <button className="text-lg">HOME</button>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/Sobre">
-                        <button>
-                            <span>
-                                SOBRE
-                            </span>
-                        </button>
-                    </Link>
+                    <Link href="/Sobre" >
+                        <button className="text-lg">SOBRE</button>
+                    </Link> 
                 </li>
                 <li>
-                <Link href="/Pesquisa">
-                        <button>
-                            <span>
-                                PESQUISA FILTRADA
-                            </span>
-                        </button>
+                    <Link href="/Pesquisa" >
+                        <button className="text-lg">PESQUISA FILTRADA</button>
                     </Link>
                 </li>                       
             </ul>
         </header>
-    )
+    );
 }
