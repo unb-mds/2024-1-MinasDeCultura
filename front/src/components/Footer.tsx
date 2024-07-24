7f39cf4131b14dd89b568b52254ae5ae770cd21b
Mateushqms <mateushqms@gmail.com>
2024-07-24 11:00:48 -0300

    style:Adicionando a responsividade do footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,32 +1,28 @@
-import Logobranca from '@/assets/logobranca.png';
 import Image from "next/image";
 import ImgGit from '@/assets/imggit.png';
 
 const Footer = () => {
   return (
-    <footer className="h-12 bg-primary-red p-40">
+    <footer className=" bg-primary-red p-14">
       <div>
-        <Image className="-mt-20 mr-4"
-          src={Logobranca}
-          alt='logobranca'
-          />
+        <h1 className="text-white lg:text-4xl sm:text-3xl text-2xl -mt-6"><strong>Minas</strong>deCultura</h1>
       </div>
-        <ul className="flex gap-10 font-sans text-2xl bg-primary-red text-white -mt-6 ml-96">
-                    <li>
+        <ul className="flex gap-10 font-sans text-2xl bg-primary-red text-white -mt-8 ml-96">
+                    <li className="hidden lg:flex">
                         <button>
                             <span>
                                 HOME
                             </span>
                         </button>
                     </li>
-                    <li>
+                    <li className="hidden lg:flex">
                         <button>
                             <span>
                                 SOBRE
                             </span>
                         </button>
                     </li>
-                    <li>
+                    <li className="hidden lg:flex">
                         <button>
                             <span>
                                 PESQUISA FILTRADA
@@ -36,15 +32,15 @@ const Footer = () => {
                 </ul>
                 
                   <a href="https://github.com/unb-mds/2024-1-MinasDeCultura" target="_blank" rel="noopener noreferrer">
-                  <Image className="-mt-14"
+                  <Image className="lg:-mt-14 -mt-4"
                   src={ImgGit}
                   alt='imggit'
-                  style={{marginLeft:'95%'}}
+                  style={{marginLeft:'96%'}}
                   />
                   </a>
-                  <div className="w-full h-px bg-white mt-20"></div>
+                  <div className="w-full h-px bg-white mt-20 hidden sm:flex"></div>
                   <div className="flex justify-end">
-                   <p className="text-white mt-10">© 2024. All Rights Reserved.</p>
+                   <p className="text-white mt-10 hidden sm:flex">© 2024. All Rights Reserved.</p>
                   </div>
                 
     </footer>
