bb78f9007d24635976bd7503f8b419d35c590713
devMarcosVM <marcosvieira.2003@gmail.com>
2024-07-26 18:11:31 -0300

    style: atualização das cores dos textos da Home, removendo filtro da Home e modificando texto do componente Filtro

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -12,22 +12,21 @@ const Grafico = dynamic(() => import('@/components/Grafico'), { ssr: false });
 export default function Home() {
   return (
     <main className="bg-primary-gray flex flex-col items-center min-h-screen gap-10 p-12">
-      <Filtro />
       <div className="container font-sans">
       <Slider/>
-        <h1 className=" text-4xl text-center">
+        <h1 className="text-neutral-700 text-4xl text-center">
           A Secretaria de Cultura e Turismo do Estado de Minas Gerais:
         </h1>
         <div className="flex md:flex-row flex-col py-6 gap-6 justify-items-center">
-          <div className="w-full h-auto bg-primary-white rounded-2xl text-center p-8 ">
+          <div className="w-full h-auto text-neutral-700 bg-primary-white rounded-2xl text-center p-8 ">
             <h1 className="text-4xl  "> Deve preservar</h1>
             <h2 className="text-2xl"> o patrimônio cultural do estado de Minas gerais</h2>
           </div>
-          <div className="w-full h-auto bg-primary-white rounded-2xl text-center p-8 ">
+          <div className="w-full h-auto text-neutral-700 bg-primary-white rounded-2xl text-center p-8 ">
             <h1 className="text-4xl "> Deve promover</h1>
             <h2 className="text-2xl"> a acessibilidade e inclusão social à cultura</h2>
           </div>
-          <div className="w-full h-auto bg-primary-white rounded-2xl text-center p-8">
+          <div className="w-full h-auto text-neutral-700 bg-primary-white rounded-2xl text-center p-8">
             <h1 className="text-4xl "> Deve fomentar</h1>
             <h2 className="text-2xl"> a produção artística da população</h2>
           </div>
@@ -35,8 +34,8 @@ export default function Home() {
       </div>
       <div className="container w-full h-auto bg-primary-white rounded-lg ">
       </div>
-      <Grafico/>
-      <Busca/>
+      {/*<Grafico/>
+      <Busca/>*/}
     </main>
   );
 }
