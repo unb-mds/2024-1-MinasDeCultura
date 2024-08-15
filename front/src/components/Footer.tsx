e9bc90f0bb52eb830df0e20c5d3c8f995fc9adbe
Mateushqms <mateushqms@gmail.com>
2024-08-15 16:54:42 -0300

    style: Pequenos ajustes nos componentes Header e Footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className=" bg-primary-red p-14">
+    <footer className=" bg-primary-red p-10">
       <div>
-        <h1 className="text-white lg:text-4xl sm:text-3xl text-2xl -mt-6"><strong>Minas</strong>deCultura</h1>
+        <h1 className="text-white lg:text-4xl sm:text-3xl text-2xl -mb-16 flex justify-initial"><strong>Minas</strong>deCultura</h1>
       </div>
-        <ul className="flex gap-10 font-sans text-2xl bg-primary-red text-white -mt-6 ml-96">
+        <ul className="flex gap-10 font-sans text-2xl bg-primary-red text-white mt-8 ml-96">
             <li className="hidden lg:flex">
                 <Link href="/">
                     <button className="text-lg">HOME</button>
@@ -24,15 +24,17 @@ const Footer = () => {
                     <button className="text-lg">PESQUISA FILTRADA</button>
                 </Link>
             </li>                   
-        </ul>       
-        <a href="https://github.com/unb-mds/2024-1-MinasDeCultura" target="_blank" rel="noopener noreferrer">
-          <Image className="lg:-mt-14 -mt-4"
-            src={ImgGit}
-            alt='imggit'
-            style={{marginLeft:'96%'}}
-          />
-        </a>
-        <div className="w-full h-px bg-white mt-20 hidden sm:flex"></div>
+        </ul>
+        <div className="flex justify-end">       
+          <a href="https://github.com/unb-mds/2024-1-MinasDeCultura" target="_blank" rel="noopener noreferrer">
+            <Image className="lg:-mt-12 lg:mb-10 -mt-4"
+              src={ImgGit}
+              alt='imggit'
+              
+            />
+          </a>
+        </div>
+        <div className="w-full h-px bg-white hidden sm:flex"></div>
         <div className="flex justify-end">
           <p className="text-white mt-10 hidden sm:flex">© 2024. All Rights Reserved.</p>
         </div>
