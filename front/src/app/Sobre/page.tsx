57dea3e28690fe9d901852ae27fa237045170cf3
Manuella Valadares <manuella.valadares@hotmail.com>
2024-08-15 10:07:43 -0300

    feat: implementa modo noturno nas três páginas do site

--- front/src/app/Sobre/page.tsx
+++ front/src/app/Sobre/page.tsx
@@ -2,7 +2,7 @@ import Informacoes from '@/components/Informacoes';
 
 export default function Sobre() {
     return (
-        <main className="bg-primary-gray flex justify-center items-center min-h-screen gap-36 p-5 lg:p-12">  
+        <main className="bg-primary-gray dark:bg-neutral-900 flex justify-center items-center min-h-screen gap-36 p-5 lg:p-12">  
         <Informacoes/>      
         </main>
     );
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,26 +1,26 @@
 "use client";
 
 import { Search, MapPin, CalendarClock, MoveRight } from "lucide-react";
-import Busca from "@/components/Busca"
+import Busca from "@/components/Busca";
 import Filtro from "@/components/Filtro";
 import dynamic from 'next/dynamic';
-import Slider from "@/components/Slider"
-import Pilares from "@/components/Pilares"
-
+import Slider from "@/components/Slider";
+import Pilares from "@/components/Pilares";
 
 const Grafico = dynamic(() => import('@/components/Grafico'), { ssr: false });
 
 export default function Home() {
   return (
-    <main className="bg-primary-gray flex flex-col items-center min-h-screen gap-10 p-12">
+    <main className="bg-primary-gray dark:bg-neutral-900 flex flex-col items-center min-h-screen gap-10 p-12">
       <div className="container font-sans">
-      <Slider/>
-      <Pilares/>
+        <Slider />
+        <Pilares />
       </div>
-      <div className="container w-full h-auto bg-primary-white rounded-lg ">
+      <div className="container w-full h-auto bg-primary-white dark:bg-neutral-800 rounded-lg">
+        {/* Conteúdo adicional pode ir aqui */}
       </div>
-      <Grafico/>
-      <Busca/>
+      <Grafico />
+      <Busca />
     </main>
   );
 }
