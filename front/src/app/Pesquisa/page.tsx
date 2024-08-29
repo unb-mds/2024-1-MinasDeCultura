fd99d0e963753ef056af6b6fb4a74e9b6267b614
Mateushqms <mateushqms@gmail.com>
2024-08-29 16:34:24 -0300

    feat: Adicionando o novo modelo de grafico

--- front/src/app/Pesquisa/page.tsx
+++ front/src/app/Pesquisa/page.tsx
@@ -1,9 +1,11 @@
 import Filtro from "@/components/Filtro";
+import Dashboard from '@/components/Dashboard';
 
 export default function Pesquisa() {
     return (
         <main className="bg-primary-gray dark:bg-neutral-900 flex flex-col items-center min-h-screen gap-10 p-12">
             <Filtro />
+            <Dashboard/>
         </main>
     )
 }
